c5513effc42d309a00c65bc8f1d3a9f1c647638f
DilipSingh <dilip@Dilip-MBA.fritz.box>
2023-09-12 00:37:53 +0200

    added helmet and morgan depdendencies

--- src/server.config.ts
+++ src/server.config.ts
@@ -1,7 +1,16 @@
 import express, { Application } from "express"
+import helmet from "helmet"
+import morgan from "morgan"
 
 const ExpressConfig = (): Application => {
   const app = express()
+
+  app.use(express.urlencoded({ extended: true }))
+  app.use(express.json())
+
+  app.use(helmet())
+  app.use(morgan("dev"))
+
   return app
 }
 export default ExpressConfig
--- src/server.ts
+++ src/server.ts
@@ -1,6 +1,13 @@
+// import { Request, Response } from "express"
 import ExpressConfig from "./server.config"
 
 const app = ExpressConfig()
 const PORT = process.env.PORT || 4000
+
+app.use("/", () => {
+  // request: Request, response: Response
+  console.log("Empty request test....");
+  
+})
 
 app.listen(PORT, () => console.log("Server Running on Port" + PORT))
